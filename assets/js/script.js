88fe2bd638c1acb73b1f22a328d650baa062c1dc
Skylar Fynboh <114550923+S7440@users.noreply.github.com>
2022-11-14 23:54:18 -0600

    Ask BCS

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+//Welcome to the game
+
+//clicking start button starts game
+
+let startButton = document.getElementById('#start-button');
+let questions = [
+    {title: "the question place holder",
+    choices: ["the choices placeholder", "anotha one"],
+    answer: "anser placeholder"}
+];
+//Button has been clicked. Begin pain sequence.
+function startGame(event) {
+    event.preventDefault();
+
+    startTimer();
+    showQuestion();
+};
+let timerEl = document.getElementById('#timer-count');
+let timeLeft = 120
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timeLeft--;
+      timerElement.textContent = timeLeft;
+      if (timeLeft >= 0) {
+        // Tests if win condition is met
+        if (noQuestion && timeLeft > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          endGame();
+        }
+      }
+    //if timer is 0 "like, totally, game over man" ~shaggy
+    if (timeLeft === 0) {
+        // Clears interval
+        clearInterval(timer);
+        endGame();
+      }
+    }, 1000);
+  }
+  //question, not to be confused with questions. the decision was questionable.
+let question  = document.getElementById('#question');
+let answer1 = document.getElementById('#answer1');
+let answer2 = document.getElementById('#answer2');
+let answer3 = document.getElementById('#answer3');
+let answer4 = document.getElementById('#answer4');
+
+function showQuestion() {
+ 
+    //display  question with multiple choice answer
+    //if answered incorrectly, "he chose... poorly" take 20 secondddddddd 
+    //if answered correctly,  "you chose... wisely" give 20  nice
+}
+
+function endGame () {
+    //if timer = 0 bad time you lose
+    //if questionsLeft === 0 you also maybe lose. Mostly your pride if your initials aren't A.S.S.
+    //ends game
+}
+let initial = document.querySelector('#initials');
+//nice is points or somethin like that. 
+let nice = document.querySelector('#nice');
+
+function highScore() {
+    //if score in top 10  good job
+    //save score in local if nice >  last stored in high scorecause we own software not rent it in this house.
+    //if nice gets stored also store initials in local cause obviously we can't afford to spend any more quarters at the arcade... damn you hydrothunder
+    //else touch grass.
+    
+}
+
+//addEventListener for start game
+//addEventListener for answer button
+
+startButton.addEventListener('click', function() {
+    startGame();
+});
+
