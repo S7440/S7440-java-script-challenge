c331671ea04b79eb86bb29b28851d31b10de725d
Skylar Fynboh <114550923+S7440@users.noreply.github.com>
2022-11-15 19:44:37 -0600

    Cant figure out and bcs is down???? :(

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,103 +1,119 @@
 //Welcome to the game
 
 //clicking start button starts game
-const startButton = document.getElementById('start-button');
 let questions = [
     {title: "the question place holder",
-    bad: ["the choices placeholder", "anotha one"], //this is the no that gives you unhappy lose time.
+    choices: ["the choices placeholder", "anotha one", "the correct one", "wrongagain"], //this is the no that gives you unhappy lose time.
     answer: "anser placeholder"},//this is the yay that gives you 20 nice
     {title: "the question place holder",
-    bad: ["the choices placeholder", "anotha one"], //this is the no that gives you unhappy lose time.
+    choices: ["the choices placeholder", "anotha one", "the correct one", "wrongagain"], //this is the no that gives you unhappy lose time.
     answer: "anser placeholder"},//this is the yay that gives you 20 nice
     {title: "the question place holder",
-    bad: ["the choices placeholder", "anotha one"], //this is the no that gives you unhappy lose time.
+    choices: ["the choices placeholder", "anotha one", "the correct one", "wrongagain"], //this is the no that gives you unhappy lose time.
     answer: "anser placeholder"},//this is the yay that gives you 20 nice
     {title: "the question place holder",
-    bad: ["the choices placeholder", "anotha one"], //this is the no that gives you unhappy lose time.
-    answer: "anser placeholder"},//this is the yay that gives you 20 nice 
-    {title: "the question place holder",
-    bad: ["the choices placeholder", "anotha one"], //this is the no that gives you unhappy lose time.
-    answer: "anser placeholder"},  //this is the yay that gives you 20 nice
-    {title: "the question place holder",
-    bad: ["the choices placeholder", "anotha one"], //this is the no that gives you unhappy lose time.
-    answer: "anser placeholder"},//this is the yay that gives you 20 nice 
-    {title: "the question place holder",
-    bad: ["the choices placeholder", "anotha one"], //this is the no that gives you unhappy lose time.
+    choices: ["the choices placeholder", "anotha one", "the correct one", "wrongagain"], //this is the no that gives you unhappy lose time.
     answer: "anser placeholder"},//this is the yay that gives you 20 nice
     {title: "the question place holder",
-    bad: ["the choices placeholder", "anotha one"], //this is the no that gives you unhappy lose time.
+    choices: ["the choices placeholder", "anotha one", "the correct one", "wrongagain"], //this is the no that gives you unhappy lose time.
+    answer: "anser placeholder"},//this is the yay that gives you 20 nice
+    {title: "the question place holder",
+    choices: ["the choices placeholder", "anotha one", "the correct one", "wrongagain"], //this is the no that gives you unhappy lose time.
     answer: "anser placeholder"},//this is the yay that gives you 20 nice
 ];
 
 
-
-
+let questionIndex = 0;
+let timer = document.querySelector('#timer-count')
+let timeEL = document.getElementById('time');
+let timeLeft = 60;
+let timerID;
+let questionsEl = document.getElementById('questions');
+let choicesEl = document.getElementById('choices');
+let submit = document.getElementById('submit-button');
+let startButton = document.getElementById('start-button');
+let initialsEl = document.getElementById('initials');
 
 //Button has been clicked. Begin pain sequence.
-function startGame(event) {
+function startGame() {
+    let startScreenEl = document.getElementById('start-screen');
+
+    //this hides the startscreen when you start the game. Currently works which is neat.
+    startScreenEl.setAttribute('class', 'hide');
+    //this is supposed to unhide the actual questions area.
+    questionsEl.removeAttribute('class', 'hide');
+    //this sets the timer interval to 1000ms on startTimer
+    timerID = setInterval(startTimer, 1000);
+    //print out your time left for the user to see how much pain is left.
+    timeEL.textContent = timeLeft;
     
-
-    startTimer();
-   // showQuestion();
+    // this is what actually loads in the questions and stuff.
+   showQuestion();
 };
-let timer = document.querySelector('#timer-count')
-let timerEl = document.getElementById('timer-count');
-let timeLeft = 10
 
+//le timer *french noises... you know the one*
 function startTimer() {
-    // Sets timer
-    timer = setInterval(function() {
-      timeLeft--;
-      timerEl.textContent = timeLeft;
-    //  if (timeLeft >= 0) {
-        // Tests if no questionsLeft
-       /* if (noQuestion && timeLeft > 0) {
-          // Clears interval and stops timer
-          timerEl.textContent = timeLeft
-          clearInterval(timer);
-          endGame();
-        }
-      }*/
-    //if timer is 0 "like, totally, game over man" ~shaggy
-    if (timeLeft === 0) {
-        // Clears interval
-        clearInterval(timer);
-     //   endGame();
-      }
-    }, 1000);
+    //increments down every cycle
+   timeLeft--;
+   //gotta let em know they have one less time
+   timeEL.textContent = timeLeft;
+   //this ends the game if they are in too much pain.
+   if (timeLeft <= 0) {
+    endGame()
+   }
   }
 
 
-  //question, not to be confused with questions. the decision was questionable.
-let question  = document.getElementById('question');
-let answer1 = document.getElementById('answer1');
-let answer2 = document.getElementById('answer2');
-let answer3 = document.getElementById('answer3');
-let answer4 = document.getElementById('answer4');
-
-const finalQuestion = questions.length -1;
-let currentQuestion = 0;
-
+//questionable questions function. displays questions
 function showQuestion() {
- 
-    let q = questions[currentQuestion];
-    //display  question with multiple choice answer
-    question.innerHTML = "<p>" + q.title + "</p>";
-    answer1.innerHTML = q.answer1;
-    answer2.innerHTML = q.answer2;
-    answer3.innerHTML = q.answer3;
-    answer4.innerHTML = qanswer4;
-    //if answered incorrectly, "he chose... poorly" take 20 secondddddddd 
-    //if answered correctly,  "you chose... wisely" give 20  nice
-    checkAnswer()
+    //the current question variable equals the questions[the selected question.]     might figure out how to randomly choose one if I have time to polish.
+    let currentQuestion = questions [questionIndex];
+    //this will come in handy with displaying the question in html
+    let titleEl = document.getElementById('question')
+    //the actual thing that prints the question in question to the question user sees
+    titleEl.textContent = currentQuestion.title;
+    //sets empty string for choices
+    choicesEl.innerHTML = "";
+
+    //this do hickee is the fun part that actually displays the choices  on the page
+    for (var i = 0; i<currentQuestion.choices.length; i++) {
+        let choice = currentQuestion.choices [i];
+        let choiceNode = document.createElement("button");
+
+        choiceNode.setAttribute('class', 'choice');
+        choiceNode.setAttribute('value', choice);
+        choiceNode.textContent = i +1 +'. ' + choice
+        choicesEl.appendChild(choiceNode)
+    }
+    
 }
 
-function checkAnswer () {
+function checkAnswer (event) {
+    let buttonEl = event.target;
+    if (!buttonEl.matches('.choice')) {
+        return
+    }
+    if (buttonEl.value !== questions[questionIndex].answer) {
+        timeLeft -= 20;
+        if (timeLeft<0) {
+            timeLeft=0;
+        }
+        timeEL.textContent = timeLeft;
 
-    if (questions[currentQuestion].correct == answer) {
-        score++
+    } 
+    questionIndex++
+    if (timeLeft <= 0 || questionIndex === questions.length) {
+        endGame()
+    } else {
+        showQuestion()
     }
 }
+
+let scoreKeeper = document.getElementById('score');
+function score()  {
+
+}
+
 function endGame () {
     //if timer = 0 bad time you lose
     //if questionsLeft === 0 you also maybe lose. Mostly your pride if your initials aren't A.S.S.
