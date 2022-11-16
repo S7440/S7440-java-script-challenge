88afc25722c0cbb74cbdc533364668b697feaf9b
Skylar Fynboh <114550923+S7440@users.noreply.github.com>
2022-11-16 10:54:27 -0600

    Askbcs

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,35 +75,46 @@ function showQuestion() {
     //sets empty string for choices
     choicesEl.innerHTML = "";
 
-    //this do hickee is the fun part that actually displays the choices  on the page
+    //this do hickee is the fun part that actually displays  the game shizz
+    //the counter guy. thank him for his service. As long as index is less than the length of the current questions choice array this for loop will do it's thang.
     for (var i = 0; i<currentQuestion.choices.length; i++) {
         let choice = currentQuestion.choices [i];
         let choiceNode = document.createElement("button");
-
+        //sets the attributes for the newly created choice button
         choiceNode.setAttribute('class', 'choice');
         choiceNode.setAttribute('value', choice);
+        //inputs the text content from the current postion in the answers
         choiceNode.textContent = i +1 +'. ' + choice
+        //busts through the html's page like the koolaid man with a brand new button for us.
         choicesEl.appendChild(choiceNode)
     }
     
 }
-
+//ensuring that we answer this shizz  correctly ideally. If not you're gonna have a bad time.
 function checkAnswer (event) {
     let buttonEl = event.target;
+    //if the you click somewhere that is not have the choice class just no. no stuff for you.
     if (!buttonEl.matches('.choice')) {
         return
-    }
+    }//this if is for if the button you click is not listed as answer you give me 20 seconds. theyre mine now thank you. I shall achieve immortality.
     if (buttonEl.value !== questions[questionIndex].answer) {
+        //the mine time
         timeLeft -= 20;
+        //if time less than 0 then time can only be 0.
         if (timeLeft<0) {
+            //set time to zero
             timeLeft=0;
         }
+        //let player know time left. AND NO LYING THIS TIME.
         timeEL.textContent = timeLeft;
 
     } 
+    //move to next question
     questionIndex++
+    //if time more less than or equal 0 or questions are at an end then end game. IDK what to tell you know more questions?
     if (timeLeft <= 0 || questionIndex === questions.length) {
         endGame()
+        //or i guess show next question.
     } else {
         showQuestion()
     }
